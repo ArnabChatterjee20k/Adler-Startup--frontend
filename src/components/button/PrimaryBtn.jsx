c825be7217456b1739ca9122047fc381000a80d3
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-06-25 13:14:31 +0530

    adjusted padding of primary btn

--- src/components/button/PrimaryBtn.jsx
+++ src/components/button/PrimaryBtn.jsx
@@ -1,6 +1,6 @@
 export const PrimaryBtn = ({text}) => {
   return (
-    <button class="inline-flex items-center bg-black text-white border-0 py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0">{text}</button>
+    <button class="inline-flex items-center bg-black text-white border-0 py-2 px-4 focus:outline-none rounded text-base mt-4 md:mt-0">{text}</button>
   )
 }
 PrimaryBtn.defaultProps = {
