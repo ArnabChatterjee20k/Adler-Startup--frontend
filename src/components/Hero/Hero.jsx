ba349b2dcf9e25b3761ba3279c87012ef323f504
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-06-25 15:42:11 +0530

    optimised the code of hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,8 +2,8 @@ import HeroImage from "./assets/Hero Image.svg"
 import { PrimaryBtn } from "../button/PrimaryBtn"
 export const Hero = () => {
     return (
-        <section class="body-font horizontal-container mt-8 px-4 flex-col-reverse space-y-reverse space-y-5 lg:h-[700px] lg:space-x-20">
-            <div className="flex flex-col  text-center space-y-7 lg:space-y-6 lg:mr-auto lg:ml-40 lg:text-left">
+        <section class="body-font horizontal-container px-4 flex-col-reverse space-y-reverse space-y-5 lg:flex-row  lg:space-x-20">
+            <div className="flex flex-col text-center space-y-7 lg:space-y-6 lg:text-left">
                 <div>
                     <h1 className="font-bold text-4xl lg:text-7xl mb-1">Adler</h1>
                     <h1 className="font-bold text-4xl lg:text-7xl">Digital Solutions</h1>
@@ -23,7 +23,7 @@ export const Hero = () => {
                 img is set to absolute to use the advantage of the height of image
                 img is absolute to body so that it can cover almost the whole canvas and z -index is set to -1
              */}
-            <img src={HeroImage} alt="Hero" className="w-[500px] h-[500px] lg:h-[700px] z-[-1] lg:absolute lg:right-32 lg:top-5" />
+            <img src={HeroImage} alt="Hero" className="w-[500px] h-[650px]" />
         </section >
     )
 }
