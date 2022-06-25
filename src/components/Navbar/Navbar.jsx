26b6c782227ea7c71e46ca6ddace9f005b19f6f8
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-06-25 13:09:30 +0530

    added secondary text to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,10 +10,10 @@ export const Navbar = () => {
                     </h2>
                 </a>
                 <nav class="md:mr-auto md:ml-4 md:pl-4 flex flex-wrap items-center justify-center text-base">
-                    <a class="mr-5 hover:text-[#989898] cursor-pointer">Services</a>
-                    <a class="mr-5 hover:text-[#989898] cursor-pointer">Reviews</a>
-                    <a class="mr-5 hover:text-[#989898] cursor-pointer">About Us</a>
-                    <a class="mr-5 hover:text-[#989898] cursor-pointer">Contact Us</a>
+                    <a class="mr-5 hover:secondary-text cursor-pointer">Services</a>
+                    <a class="mr-5 hover:secondary-text cursor-pointer">Reviews</a>
+                    <a class="mr-5 hover:secondary-text cursor-pointer">About Us</a>
+                    <a class="mr-5 hover:secondary-text cursor-pointer">Contact Us</a>
                 </nav>
                 <button class="inline-flex items-center bg-black text-white border-0 py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0">Message</button>
             </div>
