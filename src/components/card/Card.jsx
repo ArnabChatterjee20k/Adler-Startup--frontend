72e55566dc1ddb2e2169abc9e09ffc38cdc4de47
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-06-28 12:18:52 +0530

    made some customisation to card component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,13 +4,15 @@ export const Card = ({ static_card,lottie_animation, bg, heading, description, w
     return (
         <>
             {/* since we will be using this card component in the carsouel so for that static_card condition is used */}
-            <div className={`flex flex-col items-center ${bg} py-4 px-6 ${static_card?"max-w-xs sm:max-w-sm":null} shadow-md gap-2 text-left rounded`}>
-                {lottie_animation}
+            <div className={`flex flex-col items-center ${bg} py-4 px-6 ${static_card?"max-w-sm sm:max-w-md":null} shadow-md gap-2 text-left rounded`}>
+                <div className="w-full h-[5rem] flex justify-center items-center">
+                    {lottie_animation}
+                </div>
                 <div className="flex flex-col gap-3">
                     <h1 className="font-bold text-2xl">{heading}</h1>
                     <p>{description}</p>
                 </div>
-                <a href={website_link} className="text-[#611B8B] font-bold text-2xl mt-3 hover:text-[#7727aa] hover:underline duration-300">{website_name}</a>
+                <a href={website_link} className="text-[#611B8B] font-bold text-xl mt-3 hover:text-[#7727aa] hover:underline duration-300">{website_name}</a>
             </div>
         </>
     )
