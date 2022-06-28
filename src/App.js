5c283aee11cd92b23caaa298c68f09b613cac780
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-06-28 12:45:21 +0530

    Added testimonial section in the App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Navbar } from "./components/Navbar/Navbar";
 import { Hero } from "./components/Hero/Hero";
+import { Testimonial } from "./components/Testimonials/Testimonial";
 function App() {
   return (
     <>
     <Navbar/>
     <Hero/>
+    <Testimonial/>
     </>
   );
 }
