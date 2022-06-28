0b1d05aa790e36d5bd0252a759db3d66ea258408
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-06-28 13:36:50 +0530

    added Social Media Badges

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Navbar } from "./components/Navbar/Navbar";
 import { Hero } from "./components/Hero/Hero";
 import { Testimonial } from "./components/Testimonials/Testimonial";
 import { About } from "./components/About us/About";
+import { SocialMedia } from "./components/SocialBadge/SocialMedia";
 function App() {
   return (
     <>
@@ -9,6 +10,7 @@ function App() {
     <Hero/>
     <Testimonial/>
     <About/>
+    <SocialMedia/>
     </>
   );
 }
---
+++ src/components/SocialBadge/SocialMedia.jsx
@@ -0,0 +1,23 @@
+import { FaFacebookF ,  FaTwitter , FaInstagram , FaYoutube} from "react-icons/fa"
+import React from 'react'
+
+export const SocialMedia = () => {
+    const size = undefined;
+    const color = "#989898";
+    return (
+        <section className="horizontal-container space-x-10 md:space-x-20 lg:space-x-30 my-3">
+            <a href="">
+                <FaFacebookF max className={`text-4xl md:text-5xl text-[#989898] hover:text-orange-500 duration-300 hover:-translate-y-1`}/>
+            </a>
+            <a href="">
+                <FaTwitter className={`text-4xl md:text-5xl text-[#989898] hover:text-orange-500 duration-300 hover:-translate-y-1`}/>
+            </a>
+            <a href="">
+                <FaInstagram className={`text-4xl md:text-5xl text-[#989898] hover:text-orange-500 duration-300 hover:-translate-y-1`}/>
+            </a>
+            <a href="">
+                <FaYoutube className={`text-4xl md:text-5xl text-[#989898] hover:text-orange-500 duration-300 hover:-translate-y-1`}/>
+            </a>
+        </section>
+)
+}
