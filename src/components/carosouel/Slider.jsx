7653d28371a9e3b88547d786e9209303bfeb6833
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-06-28 12:16:56 +0530

    Added some customisations according to one slider or more slide to slider

--- src/components/carosouel/Slider.jsx
+++ src/components/carosouel/Slider.jsx
@@ -1,11 +1,11 @@
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import { breakpoints } from './breakpoints';
-export const Slider = ({children}) => {
-    const responsive = breakpoints();
+export const Slider = ({children,custom_breakpoints,one_card_per_slide}) => {
+    const responsive = custom_breakpoints || breakpoints();
     return(
         // here itemClass is resembling the li and cards are present inside that
-        <Carousel containerClass='py-10  max-w-[100vw]' itemClass='px-4' responsive={responsive} showDots autoPlay swipeable draggable infinite>
+        <Carousel containerClass={`py-10 ${one_card_per_slide?'sm:max-w-[400px]':'md:w-[100vw]'} w-[100%] h-lg z-0`} itemClass='px-4' sliderClass="px-1" responsive={responsive} showDots autoPlay swipeable draggable infinite>
             {children}
         </Carousel>
 
