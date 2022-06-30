73459d98fae4a2ff7d56fee8400750ee4052a870
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-06-30 19:02:39 +0530

    optimised contact page

--- src/components/Contact Us/Contact.jsx
+++ src/components/Contact Us/Contact.jsx
@@ -4,12 +4,13 @@ import { Player } from '@lottiefiles/react-lottie-player'
 import { PrimaryBtn } from "../Button/PrimaryBtn"
 import { useOptions } from "./useOptions"
 import { Options } from "./Options"
+import { Map } from "../Maps/Map"
 export const Contact = () => {
     const contactRef = useRef()
     const options_array = useOptions()
     return (
         <>
-            <section className='flex justify-center items-center flex-col space-y-3  px-4 mt-8'>
+            <section className='flex justify-center items-center flex-col space-y-3 mt-8'>
                 <div className="flex flex-col items-center justify-center">
                     <div className='flex justify-center items-center mx-auto'>
                         <h1 className='text-3xl md:text-4xl font-bold'>Contact Us</h1>
@@ -25,13 +26,12 @@ export const Contact = () => {
                     </div>
                     <PrimaryBtn text="Message Us!" classes={"w-full font-bold"} />
                 </div>
-                <div>
-
-                    <div className="px-3 flex flex-col space-y-4 justify-center items-start md:space-y-0 md:flex-row  md:space-x-10 bg-[#111111ef] text-white py-16 w-screen">
+                
+                    <div className="px-3 flex flex-col space-y-4 justify-center items-start md:space-y-0 md:flex-row  md:space-x-10 bg-[#111111ef] text-white py-16 min-w-full">
                         {options_array.map(({ icon, details }) => <Options icon={icon} details={details} />)}
                     </div>
-                </div>
             </section>
+            {/* <Map/> */}
         </>
     )
 }
