b6e9f28a90d2fe340758f028a5daaea1f6043af7
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-06-30 14:15:12 +0530

    Made some customisation in primary button component

--- src/components/button/PrimaryBtn.jsx
+++ src/components/button/PrimaryBtn.jsx
@@ -1,6 +1,6 @@
-export const PrimaryBtn = ({text}) => {
+export const PrimaryBtn = ({text,classes}) => {
   return (
-    <button class="inline-flex items-center bg-black text-white border-0 py-2 px-4 focus:outline-none rounded text-base mt-4 md:mt-0">{text}</button>
+    <button className={`inline-flex justify-center items-center bg-black text-white border-0 py-2 px-4 focus:outline-none rounded text-base mt-4 md:mt-0 ${classes}`}>{text}</button>
   )
 }
 PrimaryBtn.defaultProps = {
