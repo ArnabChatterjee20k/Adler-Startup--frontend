0e5dda0fa68d59c062970cc28855169d73d19d88
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-06-30 14:25:18 +0530

    made a map componet

---
+++ src/components/Maps/Map.jsx
@@ -0,0 +1,5 @@
+export const Map = () => {
+    return (
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5161.500028122851!2d88.13189521365466!3d25.62463933704107!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fad51ed8bd5f4f%3A0xc25fe38af34056e7!2sAshok%20Pally%2C%20East%20Netaji%20Pally%2C%20Raiganj%2C%20West%20Bengal%20733134!5e0!3m2!1sen!2sin!4v1656403892100!5m2!1sen!2sin" className="w-full h-[500px] md:h-[600px]" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    )
+}
