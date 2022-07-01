421c550948dd893c8cd2b782c41ec7ce9052cba2
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-07-01 09:42:29 +0530

    Added spinner in fallback in suspense

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Suspense } from "react";
+import DotLoader from "react-spinners/DotLoader"
 
 const Navbar = React.lazy(() => import("./components/Navbar/Navbar"))
 const Hero = React.lazy(() => import("./components/Hero/Hero"))
@@ -6,13 +7,14 @@ const Testimonial = React.lazy(() => import("./components/Testimonials/Testimoni
 const About = React.lazy(()=>import("./components/About us/About"))
 const SocialMedia = React.lazy(() => import("./components/SocialBadge/SocialMedia"))
 const Contact = React.lazy(() => import('./components/Contact Us/Contact'))
-
+const Service = React.lazy(()=>import("./components/Services/Service.jsx"))
 function App() {
   return (
     <>
-      <Suspense>
+      <Suspense fallback={<div className="w-full flex justify-center items-center min-h-screen"><DotLoader color="#D0021B"/></div>}>
         <Navbar />
         <Hero />
+        {/* <Service /> */}
         <Testimonial />
         <About />
         <SocialMedia />
