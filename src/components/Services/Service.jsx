8295444d16ba7d668ff90c1af4a4503f29f83667
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-07-01 11:37:40 +0530

    Added optimised slider service section

--- src/components/Services/Service.jsx
+++ src/components/Services/Service.jsx
@@ -12,11 +12,12 @@ const Service = () => {
                     <h1 className='text-center text-4xl font-bold  md:text-6xl'>How Our Agency</h1>
                     <h1 className='text-center text-4xl font-bold  md:text-6xl'>Can Help</h1>
                 </div>
-                <div className='w-screen'>
+                {/* <div className='w-screen grid md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-y-10'> */}
+                <div className='max-w-[90vw] overflow-hidden flex justify-center items-center'>
                     <Slider>
-                    {servies.map((service)=><Card static_card {...service} classes="min-h-[18rem]"/>)}
-
-                    </Slider>
+                    {servies.map((service)=><Card static_card {...service} classes="min-h-[20rem]"/>)}
+                </Slider>
+                {/* {servies.map((service)=><Card static_card {...service} classes="min-h-[18rem]"/>)} */}
                 </div>
             </div>
         </section>
