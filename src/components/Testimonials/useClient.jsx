e0a3a5bb4f2fdbd9e4c9ce9917770141edf3f4e7
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-07-01 09:24:03 +0530

    Used lazy loading component in testimonial

--- src/components/Testimonials/useClient.jsx
+++ src/components/Testimonials/useClient.jsx
@@ -1,13 +1,13 @@
 import cBuddy from "./clients/codding-min.png"
 import swastik from "./clients/swastik-min.jpeg"
 import rGroup from "./clients/R-group-min.png"
-
+import LazyImage from "../LazyImage/LazyImage"
 export default function useClient() {
     const clients = [
         {
             heading:null,
             description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo, vel fringilla est ullamcorper eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in ornare quam viverra",
-            lottie_animation:<img src={rGroup} className="max-w-[5rem]  rounded-full" alt="R-Group"/>,
+            lottie_animation:<LazyImage src={swastik} alt="swastik foundation"/>,
             website:{
                 website_name:"R-Group",
                 website_link:"#"
@@ -17,7 +17,7 @@ export default function useClient() {
         {
             heading:null,
             description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo, vel fringilla est ullamcorper eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in ornare quam viverra",
-            lottie_animation:<img className="max-w-[5rem]  rounded-full" src={swastik} alt="Swastik"/>,
+            lottie_animation:<LazyImage src={cBuddy} alt="Coding Buddy"/>,
             website:{
                 website_name:"Swastik",
                 website_link:"#"
@@ -27,7 +27,7 @@ export default function useClient() {
         {
             heading:null,
             description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo, vel fringilla est ullamcorper eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in ornare quam viverra",
-            lottie_animation:<img src={cBuddy} className="max-w-[5rem]  rounded-full" alt="Coding Buddy"/>,
+            lottie_animation:<LazyImage src={rGroup} alt="RGroup"/>,
             website:{
                 website_name:"Coding Buddy",
                 website_link:"#"
