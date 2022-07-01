4b9a63cf0e3a3e5f8c876175b16c518d1ec873a6
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-07-01 09:27:13 +0530

    Improved Lazyloading component

--- src/components/LazyImage/LazyImage.jsx
+++ src/components/LazyImage/LazyImage.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
-import data from "./Adler  website.png"
-const MyImage = ({src,classname,alt}) => (
+const MyImage = ({src,className,alt}) => (
     <div>
         <LazyLoadImage
             alt={alt}
             effect="blur"
-            className={classname||"max-w-[5rem]  rounded-full"}
+            className={className||"max-w-[5rem]  rounded-full"}
             src={src} // use normal <img> attributes as props
             />
         {/* <span>{image.caption}</span> */}
