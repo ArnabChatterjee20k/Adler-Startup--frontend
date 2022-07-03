45c031f7fdf4577c0dd42ab3a4560cb236887edc
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-07-03 09:40:01 +0530

    changed the location of toast container

--- src/App.js
+++ src/App.js
@@ -6,12 +6,25 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 // const Home = import("./Home")
 import Home from "./Home.jsx";
 const Navbar = React.lazy(() => import("./components/Navbar/Navbar"))
+import { ToastContainer } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
 
   return (
     <BrowserRouter>
       <Suspense fallback={<div className="w-full flex justify-center items-center min-h-screen"><DotLoader color="#D0021B" /></div>}>
       <Navbar/>
+      <ToastContainer
+                    position="top-right"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/adler/message" element={<Form />} />
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,5 @@
 import React, { Suspense } from "react";
 import DotLoader from "react-spinners/DotLoader"
-import { ToastContainer } from "react-toastify"
-import 'react-toastify/dist/ReactToastify.css';
 const Hero = React.lazy(() => import("./components/Hero/Hero"))
 const Testimonial = React.lazy(() => import("./components/Testimonials/Testimonial"))
 const About = React.lazy(() => import("./components/About us/About"))
@@ -13,17 +11,6 @@ const Home = () => {
     return (
         <>
             <Suspense fallback={<div className="w-full flex justify-center items-center min-h-screen"><DotLoader color="#D0021B" /></div>}>
-                <ToastContainer
-                    position="top-right"
-                    autoClose={5000}
-                    hideProgressBar={false}
-                    newestOnTop={false}
-                    closeOnClick
-                    rtl={false}
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover
-                />
                 <Hero />
                 <Service />
                 <Testimonial />
